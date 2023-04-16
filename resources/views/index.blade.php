1c0b5e76b5f4b56be57ce7198414b1b7b005099a
Lotrando <miroslav.klika@seznam.cz>
2023-04-16 14:12:09 +0200

    Sweet Alert 2 Installed

---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Index')
+
+@section('content')
+  Index
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Alert::toast('Sweet Alert 2 Ready!', 'success')->width('320px');
+    return view('index');
 });
